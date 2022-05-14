24ad648517b823f7f4f8a256ebbe8e8ee56f0174
Matt Hallman <matthallman@gmail.com>
2022-05-14 17:29:29 -0600

    db ch

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -13,6 +13,7 @@ FROM employees as e
 WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY e.emp_no;
 
+-- Print retirement_titles db
 SELECT * FROM retirement_titles;
 
 -- Use Dictinct On with Orderby to remove duplicate rows
@@ -38,5 +39,38 @@ ORDER BY count DESC;
 -- Print retiring_titles
 SELECT * FROM retiring_titles;
 
-SELECT * FROM titles;
 
+-- Challenge Module 7, Deliverable 2: Employees Eligible for Mentorship Program
+-- Retirement Eligibility
+DROP TABLE mentorship
+
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name, 
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as ti
+		ON (e.emp_no = ti.emp_no)
+WHERE de.to_date = '9999-01-01' 
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+-- Print mentorship db
+SELECT * FROM mentorship;
+
+-------------
+SELECT COUNT(title), title
+INTO mentorship_program
+FROM mentorship
+GROUP BY title
+ORDER BY count DESC;
+
+-- Print retiring_titles
+SELECT * FROM mentorship_program;
